53b8caec6d67970c155703d3ce08bc504000db28
Vladimir <volkovvova67@gmail.com>
2024-11-14 01:12:39 +0400

    chore(app): add alies

---
+++ src/app/index.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react"
+
+const App: FC = () => <h1 className="text-2xl text-red-500">Hello</h1>
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  resolve: {
+    alias: {
+      "@app": resolve(__dirname, "src/app"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@widgets": resolve(__dirname, "src/widgets"),
+      "@features": resolve(__dirname, "src/features"),
+      "@entities": resolve(__dirname, "src/entities"),
+      "@shared": resolve(__dirname, "src/shared"),
+    },
+  },
+})
